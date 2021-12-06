a3de09fa23c123fe0597da3b78514e5414e8928a
philipe_kraken <philipeacampos@hotmail.com>
2021-12-05 22:50:51 -0300

    Refactored
    Header and Footer code refactored

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import { FaLinkedinIn, FaGithub, FaLaptopCode} from 'react-icons/fa';
+import { FaLinkedinIn, FaGithub, FaLaptopCode, FaWhatsapp } from 'react-icons/fa';
 import styles from './Footer.module.css';
 
 export function Footer(){
@@ -10,7 +10,9 @@ export function Footer(){
                     <li><a href="https://github.com/PhilipeAnderson"><FaGithub /></a></li>
                     <li><a href="https://www.linkedin.com/in/philipe-anderson-de-campos-92593864/"><FaLinkedinIn /></a></li>
                     <li><a href="https://phillinks.com.br"><FaLaptopCode /></a></li>
+                    <li><a href="https://phillinks.com.br"><FaWhatsapp /></a></li>
                 </ul>
+                <p>Projects Manager <span> &copy; 2021</span></p>
             </div>
         </footer>
     )
