a89387e1c808cbcb12f865d9cb3a27eda74b09dc
philipe_kraken <philipeacampos@hotmail.com>
2021-12-06 22:44:25 -0300

    Home Reajusted

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,22 +5,17 @@ import { Button } from '../../components/Button/Button'
 export function Home() {
 	return (
 		<main className={styles.main}>
-			<h1>it's time to start !</h1>
+			<h1>Create your first project</h1>
+			<p>
+			Create, Access and Share your projects with the team!
+			It's easy it's fast!
+			</p>
+			
+			<Button name="Create Project" />
+					
 			<div className={styles.content}>
 				<img src={DrawImage} alt="Desenho de Capa" />
-				<div className={styles.text}>
-					<p>
-						Crie, Acesse e Compartilhe
-					</p>
-					<p>
-						seus projetos com a equipe!
-					</p>
-					<p>
-						É fácil é rápido!
-					</p>
-				</div>
 			</div>
-			<Button name="Create Project" />
 		</main>
 	)
 }
