0ccd517504596953d254a0964f98a2b466eef52a
philipe_kraken <philipeacampos@hotmail.com>
2021-12-07 09:56:03 -0300

    Page New Project
    Launched New Project Page

--- src/pages/NewProject/NewProject.js
+++ src/pages/NewProject/NewProject.js
@@ -1,5 +1,11 @@
+import styles from './NewProject.module.css';
+
 export function NewProject(){
     return (
-        <h1>NewProject</h1>
+        <div className={styles.container}>
+            <h1>Insert a new project</h1>
+            <p>Create a new project and add services to it</p>
+            <p>Formulario</p>
+        </div>
     )
 }
