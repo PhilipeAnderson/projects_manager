cbb88e8b23d7810bcd35f80842b6a95075aacecd
philipe_kraken <philipeacampos@hotmail.com>
2022-01-13 23:02:03 -0300

    Caught fake API data

---
+++ src/pages/Projects/ProjectCard.js
@@ -0,0 +1,8 @@
+import styles from './ProjectCard.module.css';
+//import { BsPencil, BsFillTrashFill } from 'react.icons/bs';
+
+export function ProjectCard({ id, name, budget, category, handleRemove}) {
+  return(
+    <p>{name}</p>
+  )
+}
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,27 +1,50 @@
+import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Message } from '../../Layout/Message';
+import { ProjectCard } from './ProjectCard';
 import { Container } from '../../Layout/Container';
-import { Button } from '../../components/Button/Button'
+import { Message } from '../../Layout/Message';
+import { Button } from '../../components/Button/Button';
 
 import styles from './Projects.module.css';
 
-export function Projects(){
+  export function Projects() {
 
-const location = useLocation()
-let message = ''
-if(location.state){
-  message = location.state.message
-}
+  const [projects, setProjects] = useState([]);
 
-  return(
+  const location = useLocation()
+  let message = ''
+  if (location.state) {
+    message = location.state.message
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:5000/projects', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      setProjects(data)
+    })
+    .catch(err => console.log(err));
+  }, []);
+
+  return (
     <div className={styles.projects_container}>
       <div className={styles.title_container}>
         <h1>My Projects</h1>
         <Button to="/newProject" name="Create Project" />
       </div>
-      {message && <Message msg={message} type="success"/>}
+      {message && <Message msg={message} type="success" />}
       <Container customClass="start">
-        <p>projetos</p>
+        {projects.length > 0 && projects.map(project => {
+          return(
+            <ProjectCard name={project.name}/>
+          )
+        })}
       </Container>
     </div>
   )
